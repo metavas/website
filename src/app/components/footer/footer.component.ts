e89d9381ca6b86fee692e4b68794b86735dbbb0e
Samuel Ogu <hello@samuelogu.com>
2022-03-21 09:42:10 +0100

    dark mode logo fixed

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  logo = 'assets/img/logo-dark.svg';
+
   constructor() { }
 
   menus = [
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,27 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  mode = window.localStorage.getItem('mode')
+  mode = localStorage.getItem('mode')
   root = document.getElementsByTagName('html')[0];
+  footer = document.getElementsByClassName('footer-logo')[0];
   status = false;
+
+  logo = 'assets/img/logo-dark.svg';
 
   constructor() { }
 
   ngOnInit(): void {
 
-    if (this.mode !== undefined && this.mode === 'dark') {
+    if (this.mode !== null && this.mode === 'dark') {
       this.root.classList.add('dark-mode');
+      // this.footer.classList.add('footer-dark-logo');
       this.status = true;
+      this.logo = 'assets/img/logo.svg';
     } else {
       this.root.classList.remove('dark-mode');
+      // this.footer.classList.remove('footer-dark-logo');
       this.status = false;
+      this.logo = 'assets/img/logo-dark.svg';
     }
 
   }
 
   async toggleMode() {
-    this.status ? this.root.classList.add('dark-mode') : this.root.classList.remove('dark-mode');
-    this.status ? localStorage.mode = 'dark' : localStorage.removeItem('mode');
+    if (this.status) {
+      this.root.classList.add('dark-mode');
+      localStorage.setItem('mode', 'dark');
+      this.logo = 'assets/img/logo.svg';
+    } else {
+      this.root.classList.remove('dark-mode');
+      localStorage.removeItem('mode');
+      this.logo = 'assets/img/logo-dark.svg';
+    }
   }
 
 }
