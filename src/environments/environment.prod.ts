3c26938ebefdb8f50ecf8ee583c0da215f03bb76
Samuel Ogu <hello@samuelogu.com>
2022-04-13 16:00:13 +0100

    production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  twitterUrl: 'https://twitter.com/metamasksupport',
-  discordUrl: 'https://discord.gg/Bd3Q8vdG',
+  twitter: 'https://twitter.com/metamasksupport',
+  discord: 'https://discord.gg/Bd3Q8vdG',
 };
