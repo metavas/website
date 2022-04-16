ffb6c72271216d84e011060405cae3568fe96624
Samuel Ogu <hello@samuelogu.com>
2022-04-16 18:14:42 +0100

    discord link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   twitter: 'https://twitter.com/MetaVas_Afro',
-  discord: 'https://discord.gg/Bd3Q8vdG',
+  discord: 'https://discord.gg/PcjGg27pmB',
   instagram: 'https://www.instagram.com/metavas_afro/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   twitter: 'https://twitter.com/MetaVas_Afro',
-  discord: 'https://discord.gg/Bd3Q8vdG',
+  discord: 'https://discord.gg/PcjGg27pmB',
   instagram: 'https://www.instagram.com/metavas_afro/'
 };
 
