73ef0bda9149eb43dea6400d7cac723053c42076
Samuel Ogu <hello@samuelogu.com>
2022-05-28 07:39:45 +0100

    created contact service

--- src/app/services/contact/contact.service.ts
+++ src/app/services/contact/contact.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  url = environment.apiUrl+'contact'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  send(data: any): Observable<any> {
+    return this.http.post(`${this.url}`, data, {})
+  }
+
 }
